62a1bcc17d870c86599bc1429ab5b4521d5e4367
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-05 01:51:26 +0700

    Add components and "global style"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/header/Header';
+import { Navbar } from './components/navbar/Navbar';
+import { Profile } from './components/profile/Profile';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
 }
 
 export default App;
