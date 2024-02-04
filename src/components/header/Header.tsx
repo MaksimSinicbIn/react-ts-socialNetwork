901995a7bc5c8295b90d52cf9e740ab2484fb341
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-05 01:53:12 +0700

    Create Header component and module.css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Header.module.css';
+
+export const Header = () => {
+    return (
+            <header className={style.header}>
+                <img src='https://sun9-41.userapi.com/impg/Ce2KnasDEw1QqJ7axTPjj0swk_mmZeIgln2tfQ/b71QAiRBvYU.jpg?size=248x261&quality=96&sign=52ed1ff9cb7234bded92512c1eb5cd44&type=album' alt='logo' />
+            </header>
+    );
+};
