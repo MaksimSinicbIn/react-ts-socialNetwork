4ded49043d7494f0ef2c448f4250d8842c98c8c2
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-09 19:48:16 +0700

    Create Settings component and css module

---
+++ src/components/settings/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Settings.module.css.module.css';
+
+type SettingsPropsType = {
+
+}
+
+export const Settings = (props: SettingsPropsType) => {
+    return (
+        <div>
+            Settings
+        </div>
+    );
+};
