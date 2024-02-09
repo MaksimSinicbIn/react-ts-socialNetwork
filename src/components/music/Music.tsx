8ff0bcb2078e335df5af2b1f686a51d3a3f52941
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-09 19:48:58 +0700

    Create Music component and css module

---
+++ src/components/music/Music.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Music.module.css.module.css';
+
+type MusicPropsType = {
+
+}
+
+export const Music = (props: MusicPropsType) => {
+    return (
+        <div>
+            Music
+        </div>
+    );
+};
