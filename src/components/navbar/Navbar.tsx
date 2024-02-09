b8cdeb6a3eab1a1dc5e1a60654e966d60295f8ba
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-09 19:47:45 +0700

    Refactoring Navbar component, add NavLink

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
             <nav className={style.nav}>
                 <div className={style.item}>
-                    <a>Profile</a>
+                    <NavLink to='/profile' activeClassName={style.activeLink}>Profile</NavLink>
                 </div>
-                <div className={`${style.item} ${style.active}`}>
-                    <a>Messages</a>
+                <div className={style.item}>
+                    <NavLink to='/dialogs' activeClassName={style.activeLink}>Messages</NavLink>
                 </div >
                 <div className={style.item}>
-                    <a>News</a>
+                    <NavLink to='/news' activeClassName={style.activeLink}>News</NavLink>
                 </div >
                 <div className={style.item}>
-                    <a>Music</a>
+                    <NavLink to='/music' activeClassName={style.activeLink}>Music</NavLink>
                 </div >
                 <div className={style.item}>
-                    <a>Settings</a>
+                    <NavLink to='/settings' activeClassName={style.activeLink}>Settings</NavLink>
                 </div >
             </nav>
     );
