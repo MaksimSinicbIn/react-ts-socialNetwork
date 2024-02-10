027d7ae9058ac61c322babcb350b94e46f732649
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-10 13:08:53 +0700

    Refactoring MyPosts component

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -4,17 +4,21 @@ import { Post } from './post/Post';
 
 export const MyPosts = () => {
     return (
-        <div>
-            My posts
+        <div className={style.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
-                <button>Remove</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                    <button>Remove</button>
+                </div>
             </div>
             <div className={style.posts}>
-                <Post message="Hi, how are you?" likesCounts={15}/>
-                <Post message="It is my first post" likesCounts={20}/>
-                <Post message="Hello, friend!" likesCounts={5}/>
+                <Post message="Hi, how are you?" likesCounts={15} />
+                <Post message="It is my first post" likesCounts={20} />
+                <Post message="Hello, friend!" likesCounts={5} />
             </div>
         </div>
     );
