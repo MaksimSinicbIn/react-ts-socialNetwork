082210097b85454ee5d29aa06527038e44289501
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-27 16:45:14 +0700

    Add styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,7 @@ export const Header = () => {
     return (
             <header className={style.header}>
                 <img src='https://sun9-41.userapi.com/impg/Ce2KnasDEw1QqJ7axTPjj0swk_mmZeIgln2tfQ/b71QAiRBvYU.jpg?size=248x261&quality=96&sign=52ed1ff9cb7234bded92512c1eb5cd44&type=album' alt='logo' />
+                <button>Login</button>
             </header>
     );
 };
