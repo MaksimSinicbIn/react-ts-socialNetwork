4fe9829efa1d1af53a235349a61c6e46d9cce1f1
MaksimSinicbIn <dzigen1986@gmail.com>
2024-05-03 23:43:00 +0700

    Completed lessons (50)51-52

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
 import s from './Users.module.css'
 import { UsersPropsType } from './UsersContainer';
-
+import axios from 'axios';
+import userPhoto from '../../assets/images/jizn.webp';
 
 export const Users = (props: UsersPropsType) => {
-    
-    if(props.usersPage.users.length === 0) {
-        props.setUsers(
-            [
-                { id: 1, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: true, fullName: 'Dmitry', status: 'Za pridurka menya ne derzhi', location: { country: 'Russia', city: 'Krasnoyarsk' } },
-                { id: 2, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: false, fullName: 'Tolyan', status: 'Chisti, chisti, chtob chisto bbIlo', location: { country: 'Russia', city: 'Ekaterinburg' } },
-                { id: 3, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: false, fullName: 'Serega', status: 'Copim dengi na comp', location: { country: 'Russia', city: 'Kansk' } },
-                { id: 4, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: true, fullName: 'Maksim', status: 'Hozyain svarki', location: { country: 'Russia', city: 'Krasnoyarsk' } }
-            ]
-        )
+
+    let getUsers = () => {
+        if(props.usersPage.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then((res) => {
+                    props.setUsers(res.data.items)
+                })
+    }
+        // props.setUsers(
+        //     [
+        //         { id: 1, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: true, fullName: 'Dmitry', status: 'Za pridurka menya ne derzhi', location: { country: 'Russia', city: 'Krasnoyarsk' } },
+        //         { id: 2, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: false, fullName: 'Tolyan', status: 'Chisti, chisti, chtob chisto bbIlo', location: { country: 'Russia', city: 'Ekaterinburg' } },
+        //         { id: 3, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: false, fullName: 'Serega', status: 'Copim dengi na comp', location: { country: 'Russia', city: 'Kansk' } },
+        //         { id: 4, photoUrl: 'https://sun9-64.userapi.com/impg/mQnQb3d1jJ9y9KfsDwtdb2aD2J7VyhfxBadOgg/_0yQfA3B4p8.jpg?size=241x225&quality=96&sign=67c70c59641e14bcdfd925c60b702eb9&type=album', followed: true, fullName: 'Maksim', status: 'Hozyain svarki', location: { country: 'Russia', city: 'Krasnoyarsk' } }
+        //     ]
+        // )
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.usersPage.users.map(u => {
                     const onFollowClickHandler = () => {
@@ -30,7 +38,7 @@ export const Users = (props: UsersPropsType) => {
                         <div key={u.id}>
                             <span>
                                 <div>
-                                    <img src={u.photoUrl} className={s.userAvatar} alt="" />
+                                    <img src={userPhoto} className={s.userAvatar} alt="" />
                                 </div>
                                 <div>
                                     {
@@ -42,12 +50,12 @@ export const Users = (props: UsersPropsType) => {
                             </span>
                             <span>
                                 <span>
-                                    <div>{u.fullName}</div>
+                                    <div>{u.name}</div>
                                     <div>{u.status}</div>
                                 </span>
                                 <span>
-                                    <div>{u.location.country}</div>
-                                    <div>{u.location.city}</div>
+                                    <div>{'u.location.country'}</div>
+                                    <div>{'u.location.city'}</div>
                                 </span>
                             </span>
                         </div>
