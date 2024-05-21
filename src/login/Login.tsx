c73c976bd3725cba357a38e6958da7bbf336e16f
MaksimSinicbIn <dzigen1986@gmail.com>
2024-05-21 16:40:54 +0700

    Completed lesson 75 + typing

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Field, InjectedFormProps, reduxForm } from 'redux-form';
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
 
 export const Login = () => {
+
+    const onSubmitHandler = (FormData: FormDataType) => {
+        console.log(FormData)
+    }
+
     return (
-        <h1>LOGIN</h1>
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmitHandler} />
+        </div>
     );
 };
+
+export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder='Login' name='login' component={'input'} />
+            </div>
+            <div>
+                <Field placeholder='Password' name='password' component={'input'} />
+            </div>
+            <div>
+                <Field type='checkbox' name='rememberMe' component={'input'} /> Remember me
+            </div>
+            <div>
+                <button>Log In</button>
+            </div>
+        </form>
+    );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm)
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -6,13 +6,15 @@ import { usersReducer } from './users-reducer';
 import { authReducer } from './auth-reducer';
 import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { useDispatch } from 'react-redux';
+import { reducer as formReducer} from 'redux-form';
 
 export const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
